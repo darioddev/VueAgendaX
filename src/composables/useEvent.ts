bc042483d2919b1f261f959f009e5366b8d2c1e2
Dario <dariopersonal16@gmail.com>
2024-02-23 20:04:49 +0100

    Implemented and Enhanced Data Mutation Handling

--- src/composables/useEvent.ts
+++ src/composables/useEvent.ts
@@ -1,19 +1,9 @@
-import app from '@/api/api'
 import type { TaskModelProps } from '@/interfaces/calendar'
 import { useMutation } from '@tanstack/vue-query'
 
-export const postEvent = async (event: TaskModelProps) => {
-  try {
-    const response = await app.post('/eventos', event)
-    return response.data
-  } catch (error) {
-    throw new Error('Hubo un error al crear el evento , intentelo de nuevo')
-  }
-}
-
-export const useAddEvent = (): any => {
+export const useMutationEvent = (): any => {
   const { isError, error, isSuccess, mutateAsync } = useMutation({
-    mutationFn: (newTodo: TaskModelProps) => postEvent(newTodo)
+    mutationFn: (request: () => Promise<TaskModelProps>) => request()
   })
 
   return {
@@ -23,3 +13,5 @@ export const useAddEvent = (): any => {
     mutateAsync
   }
 }
+
+export default useMutationEvent
